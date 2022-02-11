af6f252ce6e86eea755b60bf0cc6e7d55b67daf3
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-11 14:19:23 -0500

    Plantsy Code review

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [isSearch, setIsSearch] = useState(true)
+
+ function addNewPlant(newPlant) {
+    setPlants([...plants, newPlant])
+ } 
+
+ function handleClick() {
+   setIsSearch(isSearch=> (!isSearch))
+ }
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(plants=> setPlants(plants))
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <button name="remove" onClick={handleClick}>Remove Search Section</button>
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      {isSearch ? <Search /> : null}
+      <PlantList plants={plants} />
     </main>
   );
 }
